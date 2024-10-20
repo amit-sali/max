f052acf494074999f4305bc1290ec8ad6144a891
Amit Sali <amitsaliap@gmail.com>
2024-10-20 23:32:47 +0530

    pattern 9 done

--- dsa/Pattern/p1.cpp
+++ dsa/Pattern/p1.cpp
@@ -28,12 +28,84 @@ void print3(int n ){
    }
 }
 
+void print4(int n){
+    for(int i=0; i<n; i++){
+        for ( int j=0;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+
+void print5(int n){
+   for(int i=n;i>=0;i--){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+   }
+}
+
+void print6(int n){
+   for(int i=n;i>=0;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+   }
+}
+
+void print7(int n){
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+void print8(int n){
+    for(int i=n;i>=0;i--){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+void print9(int n){
+   print7(n);
+   print8(n);
+
+}
+
 int main() {
-    int n;
+   int n;
    cout << "enter value for N :";
    cin >> n ;
-  // print1(n);
-  print3(n);
+
+   print9(n);
+   
    
   
     return 0;

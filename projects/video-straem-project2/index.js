4443d68a0f3536120d0d715890ea9ebd7be6701c
amit-sali <amitsaliap@gmail.com>
2024-12-03 18:59:35 +0530

    video streaming project is added 2

--- projects/video-straem-project2/index.js
+++ projects/video-straem-project2/index.js
@@ -5,11 +5,24 @@ import {v4 as uuidv4} from "uuid"
 
 const app = express()
 
+app.use(
+    cors(
+        {
+            origin: ["http://localhost:3000","http://localhost:3000"],
+            credentials : true
+        }
+    )
+)
+
+app.use((req,res,next)=>{
+    
+})
+
 app.get('/',function(req,res){
     res.json({message:"hello chai aur code"})
 })
 
-app.listen(3000, function(){
+app.listen(8000, function(){
     console.log("app is listening at port 3000...")
 })
 

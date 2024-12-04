8939d45d0c59422b0bd1142450f19289d8ab2d86
amit-sali <amitsaliap@gmail.com>
2024-12-04 17:50:30 +0530

    backend understanding

--- projects/backend/app.js
+++ projects/backend/app.js
@@ -1,24 +1,48 @@
-const { log } = require('console')
-const http = require('http')
-                                                
-const server = http.createServer((req,res)=>{
-   if(req.url === "/about"){
-      res.end("the about page")
-   }
-   if(req.url ==="/home"){
-    res.end("home page")
-   }
-   if(req.url ==="/cat"){
-    res.end("")
-   }
+
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'))          //third party middleware
+
+app.use(express.json())         //in-built middleware
+app.use(express.urlencoded({extended:true}))  
+
+/*                             //custom middleware 
+                               //can be coded for diff according to route
+app.use((req, res, next) => {
+     console.log('Request URL:', req.url);
+     next(); // Pass control to the next middleware
+   });
+   
+*/
+  
+
+
+app.set("view engine",'ejs')
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
 })
 
-server.listen(3000)
+app.get('/about',(req,res)=>{
+    res.send("about page");
+})
 
+app.get('/contact',(req,res)=>{
+    res.send("conatact page");
+})
 
+app.post('/get-form-data',(req,res)=>{
+    console.log(req.body)
+    res.send("data received")
+})
 
-
-
+app.listen(3000)
 
 
 

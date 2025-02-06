55fc8df0731a7c6fba6a4e07adc3caff732180d3
amit-sali <amitsaliap@gmail.com>
2025-02-06 12:44:50 +0530

    react tutorial started

---
+++ projects/KG coding/demo/src/App.jsx
@@ -0,0 +1,15 @@
+import Random from "./assets/Random";
+
+function App(){
+  return <>
+  <h1>this is the main App.jsx</h1>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  
+  </> 
+}
+
+export default App;
